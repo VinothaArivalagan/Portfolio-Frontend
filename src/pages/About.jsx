c1edb288c361c2cd8a9442a24116a740b343106d
VinothaArivalagan <vinothag96@gmail.com>
2024-07-22 23:22:53 +0200

    updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,6 +11,15 @@ const AboutPage = () => {
                 I would be thrilled to discuss how my skills and background align with your team's needs. 
                 Please feel free to contact me at +31 617245241 or via email at vinothag96@gmail.com.
                 Thank you for considering my application. I look forward to the opportunity to discuss further.</p>
+
+                <p>In my free time, I enjoy a variety of hobbies that keep me balanced and inspired. I have a passion for painting, 
+                    which allows me to express my creativity and unwind. Baking is another hobby I cherish, as it combines precision 
+                    and creativity to produce delicious treats. Gardening helps me stay connected with nature and provides a sense of 
+                    accomplishment as I watch my plants grow. Practicing yoga keeps me centered and physically fit. I also love to travel, 
+                    exploring new places and cultures. In the evenings, I enjoy relaxing and watching Netflix with my partner, sharing quality time together.</p>
+                <p>gmail</p>
+                <p>linkedin</p>
+                <p>github</p>
         </div>
     )
 } 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,32 @@
 const Portfolio = () => {
 return (
+    <div className=" portfolio">
     <h1>Portfolio</h1>
     <h2>SECOND LIFE PRODUCTS</h2>
-    <p></p>
+    <p>For my project, I developed a full-stack application using the MERN stack (MongoDB, Express, React, and Node.js). 
+        The front-end is a Single Page Application (SPA) built with React, consisting of multiple views and implementing all CRUD actions.
+        The backend is a REST API built with Express.js, MongoDB, and Mongoose, which communicates with the React app. The application includes
+         four models, with one user model. It features sign-up, log-in, and log-out functionality, with encrypted passwords and authorization. 
+         Additionally, it supports social login. The entire application was deployed on Netlify, showcasing seamless integration between the 
+         front-end and back-end, and demonstrating a comprehensive full-stack development approach.</p>
+         <p>Technologies: JavaScript, React Native, MangoDB, Node.js, Express, Rest API</p>
+
+    <h2>Online Baking Workshop</h2>
+    <p>It is a Single Page Application (SPA) built using React, consisting of multiple views. This project is a web application 
+        designed for users to explore and enroll in a collection of online courses. The front-end is highly interactive and user-friendly, 
+        allowing users to navigate through various courses, view detailed information, and manage their enrollments seamlessly. 
+        The application showcases my ability to develop dynamic and responsive web applications with React, emphasizing user experience 
+        and efficient data handling.</p>
+        <p>Technologies: JavaScript, React, Node.js, Express</p>
+    
+    <h3>Tap Monkey!</h3>
+    <p>I developed a funny, exciting, and interactive monkey game using HTML, CSS, and JavaScript. This game is designed to keep players 
+        focused and engaged. I leveraged JavaScript for the game logic, ensuring a smooth and enjoyable user experience. As a testament 
+        to my frontend development skills, this project showcases my creativity and problem-solving abilities in creating a fun and 
+        educational gaming experience.</p>
+        <p>Technologies: HTML, CSS, Javascript</p>
+
+    </div>
 )
 }
+export default Portfolio;
